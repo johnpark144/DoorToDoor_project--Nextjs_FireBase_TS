2b60b8be9e7794a089bfd1bba57ddd93b07dc916
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-02-03 12:58:09 -0600

    Update store.ts

--- store/store.ts
+++ store/store.ts
@@ -30,15 +30,27 @@ const allMarkedDataSlice = createSlice({
   },
 });
 
+const isFocusedSlice = createSlice({
+  name: "isFocusedSlice",
+  initialState: false,
+  reducers: {
+    isFocusedInfo: (state: boolean, action) => {
+      return (state = action.payload);
+    },
+  },
+});
+
 export const { userInfo } = userObjSlice.actions;
 export const { dateInfo } = dateSlice.actions;
 export const { allMarkedDataInfo } = allMarkedDataSlice.actions;
+export const { isFocusedInfo } = isFocusedSlice.actions;
 
 export default configureStore({
   reducer: {
     userObjSlice: userObjSlice.reducer,
     dateSlice: dateSlice.reducer,
     allMarkedDataSlice: allMarkedDataSlice.reducer,
+    isFocusedSlice: isFocusedSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }), // To Resolve Error to save UnSerialized DateInfo
